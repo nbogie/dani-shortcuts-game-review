7eb3bfcf5f58be05b11a3dfb92504d4571b91659
dani voro <mr.voro@gmail.com>
2023-08-06 15:42:07 +0200

    creating the first elements of the game

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,36 @@
-import { MyComponent } from "./MyComponent";
+import { useState } from "react";
+import Keyboard from "./Keyboard";
 import "./App.css";
-import { greet } from "../greet";
 
 function App() {
+    const [currentLevel, setCurrentLevel] = useState<number>(0);
+    const [levelOneIsCompleted, setLevelOneIsCompleted] =
+        useState<boolean>(false);
+
+    const handleLevelChange = (level: number) => {
+        setCurrentLevel(level);
+
+        // Check if level 1 is completed
+        if (level === 1) {
+            setLevelOneIsCompleted(true);
+        }
+    };
+
     return (
         <div className="App">
-            <MyComponent />
-
-            {greet("World")}
+            <h1>Shortcuts Game</h1>
+            <h2>The current level is: {currentLevel}</h2>
+            <Keyboard onLevelChange={handleLevelChange} />{" "}
+            <p className="description">
+                To complete level 1, please use the shortcut which is used to
+                comment out the selected line.
+            </p>
+            {levelOneIsCompleted && <div>Level 1 is completed</div>}
+            <p className="description">
+                To complete level 2, please use the shortcut which is used to
+                add indentation to a selected line.
+            </p>
+            {/* {levelTwoIsCompleted && <div>Level 2 is completed</div>} */}
         </div>
     );
 }
---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Mousetrap from "mousetrap";
+
+interface KeyboardProps {
+    onLevelChange: (level: number) => void;
+}
+
+const Keyboard = ({ onLevelChange }: KeyboardProps) => {
+    const [currentLevel, setCurrentLevel] = useState<number>(0);
+
+    useEffect(() => {
+        Mousetrap.bind("ctrl+/", (e) => {
+            e.preventDefault();
+            if (currentLevel === 0) {
+                const newLevel = currentLevel + 1;
+                setCurrentLevel(newLevel);
+                onLevelChange(newLevel); // to update the parent component.
+            }
+        });
+
+        Mousetrap.bind("ctrl+[", (e) => {
+            e.preventDefault();
+            if (currentLevel === 1) {
+                const newLevel = currentLevel + 1;
+                setCurrentLevel(newLevel);
+                onLevelChange(newLevel);
+            }
+        });
+
+        return () => {
+            Mousetrap.reset();
+        };
+    }, [currentLevel, onLevelChange]);
+
+    return <div>A game to prove your ability with the keyboard!</div>;
+};
+
+export default Keyboard;
