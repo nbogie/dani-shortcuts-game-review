d3376b5075f878327833c3bb6d4541a944bdf6ba
dani voro <mr.voro@gmail.com>
2023-08-06 22:49:56 +0200

    improvements in condition rendering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -28,13 +28,22 @@ function App() {
 
     function levelOneClass(): string {
         let classes = "description";
-        if (currentLevel >= 1) classes = "enable";
+        if (currentLevel === 0) classes = "enable";
+        if (currentLevel > 0) classes = "used";
         return classes;
     }
 
     function levelTwoClass(): string {
         let classes = "description";
-        if (currentLevel >= 2) classes = "enable";
+        if (currentLevel === 1) classes = "enable";
+        if (currentLevel > 1) classes = "used";
+        return classes;
+    }
+
+    function levelThreeClass(): string {
+        let classes = "description";
+        if (currentLevel === 2) classes = "enable";
+        if (currentLevel > 2) classes = "used";
         return classes;
     }
 
@@ -43,21 +52,23 @@ function App() {
             <h1>Shortcuts Game</h1>
             <h2>The current level is: {currentLevel}</h2>
             <Keyboard onLevelChange={handleLevelChange} />{" "}
-            <p className="enable">
+            <p className={levelOneClass()}>
                 To complete level 1, please use the shortcut which is used to
                 comment out the selected line.
             </p>
             {levelOneIsCompleted && <div>Level 1 is completed!</div>}
-            <p className={levelOneClass()}>
+            <p className={levelTwoClass()}>
                 To complete level 2, please use the shortcut which is used to
                 add indentation to a selected line.
             </p>
             {levelTwoIsCompleted && <div>Level 2 is completed!</div>}
-            <p className={levelTwoClass()}>
+            <p className={levelThreeClass()}>
                 To complete level 3, please use the shortcut which is used to
                 fit the text to your window.
             </p>
-            {levelThreeIsCompleted && <div>GAME COMPLETED!</div>}
+            {levelThreeIsCompleted && (
+                <div className="completed-game">GAME COMPLETED!</div>
+            )}
         </div>
     );
 }
