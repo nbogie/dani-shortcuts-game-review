9c55307a0a7ebaea5152d5ab22f659c97ced0643
dani voro <mr.voro@gmail.com>
2023-08-09 15:00:33 +0200

    adding checkin shortcut to the starting page

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Mousetrap from "mousetrap";
 
 interface StartPageProps {
     onStart: () => void;
 }
 
 const StartPage: React.FC<StartPageProps> = ({ onStart }) => {
+    const [shortcutTested, setShortcutTested] = useState(false);
+
     return (
         <div className="start-page">
             <h1>Welcome to The Shortcuts Game</h1>
             <p className="catch-phrase">
                 Prove your ability with the keyboard!
             </p>
+            <p className="start-caption">
+                Press Ctrl+a to show the start button.
+            </p>
             <div className="start-button">
-                <button onClick={onStart}>Start Game!</button>
+                {shortcutTested && (
+                    <button onClick={onStart}>Start Game!</button>
+                )}
             </div>
+            <Tests setShortcutTested={setShortcutTested} />
         </div>
     );
+};
+
+interface TestsProps {
+    setShortcutTested: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Tests: React.FC<TestsProps> = ({ setShortcutTested }) => {
+    useEffect(() => {
+        Mousetrap.bind("ctrl+a", (e) => {
+            e.preventDefault();
+            setShortcutTested(true);
+        });
+
+        return () => {
+            Mousetrap.reset();
+        };
+    }, [setShortcutTested]);
+
+    return <div></div>;
 };
 
 export default StartPage;
