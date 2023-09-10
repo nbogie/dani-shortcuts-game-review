11c2651761901ea58390f484071328cd3cd3cd69
nbogie <neillbogie@googlemail.com>
2023-09-10 12:25:49 +0000

    formatting only

--- src/components/GameInProgress.tsx
+++ src/components/GameInProgress.tsx
@@ -6,7 +6,6 @@ import { getRandomizedLevelsData } from "./LevelsData";
 import { LevelsDisplay } from "./LevelsDisplay";
 import { PressedKeysView } from "./PressedKeysView";
 
-
 export function GameInProgress() {
     const levelsData = useMemo(getRandomizedLevelsData, []);
 
